d09c540563b1615f72c829dd258f403e92ca5cba
JonyM355 <joaoricardoam05@gmail.com>
2025-11-10 14:51:42 +0000

    Doing project ExtendString

---
+++ ExtendString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace ExtendString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!".ToRandomCase());
+        }
+    }
+}
